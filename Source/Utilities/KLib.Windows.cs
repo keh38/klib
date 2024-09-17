779f4e0ce75acbd1fc89fe47d7ca2dbd6a433109
keh38 <Ken_Hancock@meei.harvard.edu>
2024-09-17 09:26:23 -0400

    added methods to Windows

--- Source/Utilities/KLib.Windows.cs
+++ Source/Utilities/KLib.Windows.cs
@@ -8,6 +8,14 @@ namespace KLib
 {
     public static class Windows
     {
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
         public class ChildControl
         {
             public string name;
@@ -42,25 +50,58 @@ namespace KLib
             GWL_EXSTYLE = (-20),
             GWL_USERDATA = (-21),
             GWL_ID = (-12)
+
         }
 
+        public enum ShowWindowFlags : int
+        {
+            ShowNormal = 1,
+            ShowMaximized = 3,
+            Show = 5,
+            Restore = 9
+        }
+
+        private enum SetWindowPosFlags : uint
+        {
+            SynchronousWindowPosition = 0x4000,
+            DeferErase = 0x2000,
+            DrawFrame = 0x0020,
+            FrameChanged = 0x0020,
+            HideWindow = 0x0080,
+            DoNotActivate = 0x0010,
+            DoNotCopyBits = 0x0100,
+            IgnoreMove = 0x0002,
+            DoNotChangeOwnerZOrder = 0x0200,
+            DoNotRedraw = 0x0008,
+            DoNotReposition = 0x0200,
+            DoNotSendChangingEvent = 0x0400,
+            IgnoreResize = 0x0001,
+            IgnoreZOrder = 0x0004,
+            ShowWindow = 0x0040,
+        }
         public delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
 
         [DllImport("user32")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr lParam);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern int GetWindowTextLength(IntPtr hWnd);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
         [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
         public static extern int GetWindowLong(IntPtr hWnd, GWL nIndex);
 
-        [DllImport("User32.dll")]
-        public static extern int SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
 
         [DllImport("user32.dll")]
         public static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
@@ -75,6 +116,21 @@ namespace KLib
             uint uTImeoutj,
             out IntPtr result);
 
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        [DllImport("User32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+        public static bool ShowWindowAsync(IntPtr hWnd, ShowWindowFlags flags)
+        {
+            return ShowWindowAsync(hWnd, (int)flags);
+        }
 
         public static string GetWindowText(IntPtr hWnd)
         {
@@ -84,7 +140,14 @@ namespace KLib
             return stringBuilder.ToString();
         }
 
-        public static void ButtonClick(IntPtr hWnd)
+        public static bool MoveWindow(IntPtr hWnd, int X, int Y)
+        {
+            Rect r = new Rect();
+            GetWindowRect(hWnd, ref r);
+            return MoveWindow(hWnd, X, Y, r.Right - r.Left, r.Bottom - r.Top, false);
+        }
+
+        public static void SimulateButtonClick(IntPtr hWnd)
         {
             SendMessage(hWnd, Windows.BM_CLICK, IntPtr.Zero, IntPtr.Zero);
         }
@@ -108,20 +171,6 @@ namespace KLib
 
             return childHandles;
         }
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        public const int SW_SHOWNORMAL = 1;
-        public const int SW_SHOWMAXIMIZED = 3;
-        public const int SW_SHOW = 5;
-        public const int SW_RESTORE = 9;
-
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 
         private static bool EnumWindow(IntPtr hWnd, IntPtr lParam)
         {
