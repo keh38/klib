a07c920cf440636654622ed754344b2005cc4378
Ken Hancock <Ken_Hancock@meei.harvard.edu>
2025-03-13 19:04:46 -0400

    added KLib.WindowsVoice

---
+++ Source/WindowsVoice/KLib.WindowsVoice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLib.WindowsVoice
+{
+    public class WindowsVoice
+    {
+        [DllImport("WindowsVoice")]
+        public static extern void initSpeech(string attrib);
+        [DllImport("WindowsVoice")]
+        public static extern void destroySpeech();
+        [DllImport("WindowsVoice")]
+        public static extern void addToSpeechQueue(string s);
+        [DllImport("WindowsVoice")]
+        public static extern void clearSpeechQueue();
+        [DllImport("WindowsVoice")]
+        public static extern void statusMessage(StringBuilder str, int length);
+
+        private static string _voiceAttrib = "";
+
+        public WindowsVoice()
+        {
+            initSpeech(_voiceAttrib);
+        }
+
+        public void Test()
+        {
+            Speak("Testing");
+        }
+
+        public void setVoice(string attrib)
+        {
+            if (!_voiceAttrib.Equals(attrib))
+            {
+                _voiceAttrib = attrib;
+                destroySpeech();
+                initSpeech(_voiceAttrib);
+            }
+        }
+
+        public void Speak(string msg)
+        {
+            addToSpeechQueue(msg);
+        }
+
+        void Shutdown()
+        {
+            destroySpeech();
+        }
+
+        public string GetStatusMessage()
+        {
+            StringBuilder sb = new StringBuilder(40);
+            statusMessage(sb, 40);
+            return sb.ToString();
+        }
+    }
+}
--- Unit Tests/KLib.Test/MainForm.cs
+++ Unit Tests/KLib.Test/MainForm.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-//using System.Linq;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-using System.Diagnostics;
+using KLib.WindowsVoice;
 
 namespace KLibUnitTests
 {
@@ -39,29 +40,8 @@ namespace KLibUnitTests
         {
             //ToneCloudDemo dlg = new ToneCloudDemo();
             //dlg.ShowDialog();
-
-            ulong t1 = 133860287475469286;
-            ulong lowMask = 0xFFFFFFFF;
-            double highInt = (double)(t1 >> 32);
-            double lowInt = (double)(t1 & lowMask);
-            ulong t2 = (ulong)(highInt * (lowMask + 1)) + (ulong)(lowInt);
-            Debug.WriteLine($"[ {highInt} {lowInt} {t1} {t2} ]");
-
-            double t3 = ((double)t1) * 1e-7;
-            Debug.WriteLine($"{t3:0.0000000}");
-
-            var scaleFactor = (float)(lowMask + 1) / 1e7;
-            Debug.WriteLine(scaleFactor);
-            double t4 = highInt;// * 1e-7;
-            t4 *= scaleFactor;// + lowInt * 1e-7;
-            Debug.WriteLine($"highInt = {highInt:0.0000000}");
-            Debug.WriteLine($"lowInt = {lowInt * 1e-7:0.0000000}");
-            Debug.WriteLine($"{t4:0.0000000}");
-
-            double wtf = 13386028364.1757698059;
-            Debug.WriteLine($"wtf = {wtf:0.000000000}");
-            wtf = 28364.1757698059;
-            Debug.WriteLine($"wtf = {wtf:0.000000000}");
+            var voice = new WindowsVoice();
+            voice.Speak("hello dude");
         }
 
         private void RandomButton_Click(object sender, EventArgs e)
