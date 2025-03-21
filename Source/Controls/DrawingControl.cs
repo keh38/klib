8ed4242accf5221463630dc86711600cc7ace014
Ken Hancock <Ken_Hancock@meei.harvard.edu>
2025-03-21 08:31:54 -0400

    added DrawingControl.cs

---
+++ Source/Controls/DrawingControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KLib.Controls
+{
+    // https://stackoverflow.com/questions/487661/how-do-i-suspend-painting-for-a-control-and-its-children
+    public class DrawingControl
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+
+        private const int WM_SETREDRAW = 11;
+
+        public static void SuspendDrawing(Control parent)
+        {
+            SendMessage(parent.Handle, WM_SETREDRAW, false, 0);
+        }
+
+        public static void ResumeDrawing(Control parent)
+        {
+            SendMessage(parent.Handle, WM_SETREDRAW, true, 0);
+            parent.Refresh();
+        }
+    }
+}
