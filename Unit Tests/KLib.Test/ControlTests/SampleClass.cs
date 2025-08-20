63f4ef3baec1e284c27420b150435ed8b04d2b1a
Ken Hancock <Ken_Hancock@meei.harvard.edu>
2025-08-20 09:51:21 -0400

    still cleaning up

---
+++ Unit Tests/KLib.Test/ControlTests/SampleClass.cs
@@ -0,0 +1,17 @@
+﻿namespace ControlTests
+{
+    public class SampleClass
+    {
+        public string Name { get; set; }
+        public float Value { get; set; }
+        public bool Other { get; set; }
+
+        public SampleClass()
+        {
+            Name = "Hello";
+            Value = 12;
+            Other = false;
+        }
+    }
+}
+
