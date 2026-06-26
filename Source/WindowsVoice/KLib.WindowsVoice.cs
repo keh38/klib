42058f04aa9e5d98012dad215ee7d8c15597503a
keh38 <Ken_Hancock@meei.harvard.edu>
2026-06-26 14:16:18 -0400

    added Render() method

--- Source/WindowsVoice/KLib.WindowsVoice.cs
+++ Source/WindowsVoice/KLib.WindowsVoice.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
 
 namespace KLib.WindowsVoice
 {
@@ -57,6 +59,27 @@ namespace KLib.WindowsVoice
             StringBuilder sb = new StringBuilder(40);
             statusMessage(sb, 40);
             return sb.ToString();
+        }
+
+        public float[] Render(string text)   // text can be SSML/markup if you want
+        {
+            using (var synth = new SpeechSynthesizer())
+            using (var mem = new System.IO.MemoryStream())
+            {
+                var fmt = new SpeechAudioFormatInfo(48000,        // match your streamer's rate
+                                AudioBitsPerSample.Sixteen, AudioChannel.Mono);
+                synth.SetOutputToAudioStream(mem, fmt);
+                // synth.SelectVoice(...) here if you need a specific language/voice
+                synth.Speak(text);                                // synchronous, faster than real time
+                synth.SetOutputToNull();
+
+                byte[] b = mem.ToArray();
+                int n = b.Length / 2;
+                var s = new float[n];
+                for (int i = 0; i < n; i++)
+                    s[i] = (short)(b[2 * i] | (b[2 * i + 1] << 8)) / 32768f;   // int16 LE → normalized float
+                return s;
+            }
         }
     }
 }
