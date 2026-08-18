77ae50fccd29badb3fa58f4cc6473e72d41a94e7
Ken Hancock <Ken_Hancock@meei.harvard.edu>
2026-08-17 23:11:56 -0400

    added WindowsVoice package

--- Source/WindowsVoice/KLib.WindowsVoice.cs
+++ Source/WindowsVoice/KLib.WindowsVoice.cs
@@ -6,61 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Synthesis;
 using System.Speech.AudioFormat;
+using System.Runtime.Versioning;
 
 namespace KLib.WindowsVoice
 {
     public class WindowsVoice
     {
-        [DllImport("WindowsVoice")]
-        public static extern void initSpeech(string attrib);
-        [DllImport("WindowsVoice")]
-        public static extern void destroySpeech();
-        [DllImport("WindowsVoice")]
-        public static extern void addToSpeechQueue(string s);
-        [DllImport("WindowsVoice")]
-        public static extern void clearSpeechQueue();
-        [DllImport("WindowsVoice")]
-        public static extern void statusMessage(StringBuilder str, int length);
-
-        private static string _voiceAttrib = "";
-
-        public WindowsVoice()
-        {
-            initSpeech(_voiceAttrib);
-        }
-
-        public void Test()
-        {
-            Speak("Testing");
-        }
-
-        public void setVoice(string attrib)
-        {
-            if (!_voiceAttrib.Equals(attrib))
-            {
-                _voiceAttrib = attrib;
-                destroySpeech();
-                initSpeech(_voiceAttrib);
-            }
-        }
-
-        public void Speak(string msg)
-        {
-            addToSpeechQueue(msg);
-        }
-
-        void Shutdown()
-        {
-            destroySpeech();
-        }
-
-        public string GetStatusMessage()
-        {
-            StringBuilder sb = new StringBuilder(40);
-            statusMessage(sb, 40);
-            return sb.ToString();
-        }
-
         public float[] Render(string text)   // text can be SSML/markup if you want
         {
             using (var synth = new SpeechSynthesizer())
